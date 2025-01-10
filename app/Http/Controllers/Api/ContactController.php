478d34b484602301183f3c4b04f853bec8c3c6c6
saadkhan3977 <xaadkhan43@gmail.com>
2025-01-10 21:03:07 +0500

    contact

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ContactInfo;
+use App\Models\Contact;
 use App\Models\AdminInfo;
 use Validator;
 class ContactController extends BaseController
@@ -19,7 +20,7 @@ class ContactController extends BaseController
                 'email' =>'required|email',
                 'subject'=>'required|string',
                 'description'=>'required|string'
-            ]); 
+            ]);
             if($validator->fails())
             {
                 return $this->sendError($validator->errors()->first());
@@ -36,7 +37,7 @@ class ContactController extends BaseController
     }
     public function admininfo()
     {
-       
+
         try{
             $admin =AdminInfo::first();
             return response()->json(['success'=>true,'data'=>$admin]);
@@ -46,4 +47,34 @@ class ContactController extends BaseController
 
         }
     }
+
+    // List all contacts
+    public function index()
+    {
+        $contacts = Contact::all();
+
+        // return response()->json($contacts, 200);
+        return response()->json(['success'=>true,'contacts_list'=>$contacts],200);
+    }
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'number' => 'required|string|max:15',
+        ]);
+
+        if($validator->fails())
+        {
+            return $this->sendError($validator->errors()->first());
+        }
+
+        $contact = Contact::create($request->all());
+
+        return response()->json([
+            'message' => 'Contact created successfully',
+            'data' => $contact
+        ], 201);
+    }
+
+
 }
